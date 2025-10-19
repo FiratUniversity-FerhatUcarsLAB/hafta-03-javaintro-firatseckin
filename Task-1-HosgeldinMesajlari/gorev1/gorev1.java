2a4fdc8b1cc99ca102b0fdbc284fc0bc07b970a4
firatseckin <250541042@firat.edu.tr>
2025-10-19 18:15:58 +0300

    Delete Task-1-HosgeldinMesajlari/gorev1 directory

--- Task-1-HosgeldinMesajlari/gorev1/gorev1.java
+++
@@ -1,7 +0,0 @@
-public class gorev1 {
-    public static void main(String[] args) {
-        System.out.println("Java'ya Hos Geldiniz");
-        System.out.println("Bilgisayar Bilimine Hos Geldiniz");
-        System.out.println("Programlama Eglencelidir");
-    }
-}
